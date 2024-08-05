1ada385782c621d5a79dea469d9166c7f75d0bd4
GLITCHRS <glitch@glitch.business>
2024-08-05 13:39:40 +0300

    init vs2022 project

---
+++ GLITCH-APP_SN/src/App.cpp
@@ -0,0 +1,19 @@
+#include "stdafx.h"
+#include <QGuiApplication>
+#include <QQmlApplicationEngine>
+
+int main(int argc, char *argv[])
+{
+#if defined(Q_OS_WIN) && QT_VERSION_CHECK(5, 6, 0) <= QT_VERSION && QT_VERSION < QT_VERSION_CHECK(6, 0, 0)
+    QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
+#endif
+
+    QGuiApplication app(argc, argv);
+
+    QQmlApplicationEngine engine;
+    engine.load(QUrl(QStringLiteral("qrc:/qt/qml/glitch-app_sn/main.qml")));
+    if (engine.rootObjects().isEmpty())
+        return -1;
+
+    return app.exec();
+}
